3004ce015f446daf500eb51257f6192918eef3b7
Jeff Purcell <japurcell@users.noreply.github.com>
2018-04-16 09:30:55 -0700

    Saving

---
+++ Psns.Common.Analysis/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Psns.Common.Analysis
+
+type Class1() = 
+    member this.X = "F#"
---
+++ Psns.Common.Analysis/Script.fsx
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+#load "Library1.fs"
+open Psns.Common.Analysis
+open System
+
+// Define your library scripting code here
+
+let calcDelta (optionalPrevious: DateTime option) =
+    match optionalPrevious with
+    | Some(prev) -> DateTime.Now.Subtract(prev).Ticks
+    | None -> TimeSpan.Zero.Ticks
+
+let apply delta = (delta * 0.001, delta * 0.999)
+
+// calcDelta |> applyBoundary |> classify
